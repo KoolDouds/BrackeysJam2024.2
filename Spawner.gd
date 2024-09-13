cc70e9ad30e985c7d6f5b11a46679fb8817c3447
Giovanni Karra <karragiovanni@gmail.com>
2024-09-13 16:37:11 +0200

    mécaniques de chute de météorites

---
+++ Spawner.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+@export var meteor := preload("res://meteor.tscn")
+var timer := 0.0
+
+func _process(delta):
+	timer += delta
+	if timer > 5:
+		timer = 0.0
+		spawn()
+
+func spawn():
+	print("SPAWN")
+	var inst := meteor.instantiate()
+	var height := randi_range(1000, 10000)
+	var target_pos := position + Vector2(randf_range(-400, 400), randf_range(-400, 400))
+	inst.position = target_pos + Vector2.UP * height
+	var fall := inst.get_node("Fall")
+	fall.target_pos = target_pos
+	get_tree().current_scene.add_child(inst)
