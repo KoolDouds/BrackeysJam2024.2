8c41dbf5906af3b76b43559a0914f9fa2ed5eddd
Caberu <lecaberudo@gmail.com>
2024-09-13 20:30:52 +0200

    EVERYTHING IS ON TVVVV

--- Prefabs/ViewPortMove.gd
+++ Prefabs/ViewPortMove.gd
@@ -1,8 +1,11 @@
 extends Node2D
 
 @export var cam_speed := 500.0 #pixel/seconde
+@export var move_enabled := true
 
 func _process(delta):
+	if (!move_enabled): return
+	
 	var dir = Vector2.ZERO
 	if (Input.is_action_pressed("ui_left")):
 		dir += Vector2.LEFT
@@ -14,3 +17,6 @@ func _process(delta):
 		dir += Vector2.DOWN
 	
 	position += dir*cam_speed*delta
+
+func set_enable_move(value := true):
+	move_enabled = value
